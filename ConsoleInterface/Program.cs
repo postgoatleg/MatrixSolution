e8afb592904900a3ea24dc691f0e152f390c890b
Andrei Kovtun <andreikovtun@MacBook-Air-Andrei.local>
2022-09-28 16:23:28 +0300

    first commit

---
+++ ConsoleInterface/Program.cs
@@ -0,0 +1,31 @@
+﻿using MatrixClass;
+
+Matrix drain = new(2, 2, GetString("Enter matrix name"));
+Matrix drain1 = new(2, 2, GetString("Enter matrix name"));
+drain.FillMatrix();
+drain1.FillMatrix();
+Console.WriteLine(drain.PrintMatrix());
+Console.WriteLine(drain1.PrintMatrix());
+Matrix drain3 = drain * drain1;
+Console.WriteLine(drain3.PrintMatrix());
+
+
+//drain.ChangeElement(GetInt("Enter column"), GetInt("Enter row"), GetInt("Enter new value"));
+//Console.WriteLine(drain.PrintMatrix());
+
+Console.ReadLine();
+
+static string? GetString(string description)
+{
+    string? rtrnStrng;
+    Console.WriteLine(description);
+    rtrnStrng = Console.ReadLine();
+    return rtrnStrng;
+}
+
+static int GetInt(string description)
+{
+    Console.WriteLine(description);
+    int retValue = Convert.ToInt32(Console.ReadLine());
+    return retValue;
+}
---
+++ MatrixClass/Matrix.cs
@@ -0,0 +1,112 @@
+﻿namespace MatrixClass;
+public class Matrix
+{
+    int a, b;
+    string? name = "";
+    private int[,] matrix;
+
+    public Matrix(int a, int b, string? name)
+    {
+        this.a = a;
+        this.b = b;
+        this.name = name;
+        matrix = new int[a, b];
+    }
+    /// <summary>
+    /// i - num of culumn, j - num of row, num - new value
+    /// </summary>
+    /// <param name="i"></param>
+    /// <param name="j"></param>
+    /// <param name="num"></param>
+    public void ChangeElement(int i, int j, int num)
+    {
+        this.matrix[i,j] = num;
+    }
+
+    public bool IsTrue()
+    {
+        int i = 0;
+        while (i < this.a)
+        {
+            int j = 0;
+            while (j < this.b)
+            {
+                if(this.matrix[i, j] != 0)
+                {
+                    return true;
+                }
+            }
+            i++;
+        }
+        return false;
+    }
+
+    public void FillMatrix()
+    {
+        for(int i = 0; i < this.a; i++)
+        {
+            for(int j =0; j < this.b; j++)
+            {
+                Console.Write($"Enter a[{i},{j}]: ");
+                this.matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+
+    public static Matrix operator *(Matrix m1, Matrix m2)
+    {
+        if (m1.a != m2.a && m1.b != m2.b)
+        {
+            return new Matrix(0, 0, "null");
+        }
+        else
+        {
+            Matrix m3 = new(m1.a, m2.b, m1.name + m2.name);
+            for (int i = 0; i<m1.a; i++)
+            {
+                for(int j = 0; j<m1.b; j++)
+                {
+                    int ii=0, jj=0, elementValue=0; 
+                    while(ii<m1.a && jj < m1.b)
+                    {
+
+                        elementValue += m1.matrix[i, jj] * m2.matrix[ii, j];
+                        ii++;
+                        jj++;
+                    }
+                    m3.matrix[i, j] = elementValue;
+                }
+            }
+            return m3;
+        }
+    }
+
+    public string PrintMatrix()
+    {
+        string resultString = $"Matrix {this.name}:\n";
+        for (int i = 0; i < this.a; i++)
+        {
+            for (int j = 0; j < this.b; j++)
+            {
+                resultString += Convert.ToString(this.matrix[i, j]) + ", ";
+
+            }
+            resultString += "\n";
+        }
+        return resultString;
+    }
+
+    //public static double getAverage(Matrix m)
+    //{
+    //    int[] copies;
+    //    bool[] test;
+    //    double count=0, sum = 0;
+    //    for(int i = 0; i<m.a; i++)
+    //    {
+
+    //    }
+
+    //}
+}
+
+
